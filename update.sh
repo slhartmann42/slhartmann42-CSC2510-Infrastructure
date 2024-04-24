1283da6d5bdf04d47f4c2119dfc4fc5049ae1504
slhartmann42 <samuelh343@gmail.com>
2024-04-24 19:20:27 +0000

    Updated shell

--- update.sh
+++ update.sh
@@ -4,10 +4,11 @@ if [[ $# -eq 0 ]]; then
 	echo 'Sorry no password given so password unchanged'
 
 else
-mysql -u root --skip-password <<EOFT
+sudo systemctl stop mysql
+sudo mysqld --skip-grant-tables <<EOFT
+FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY "$1";
-FLUSH PRIVILEDGES;
-quit;
+FLUSH PRIVILEGES;
 EOFT
 echo "Password changed and flushed"
 fi
