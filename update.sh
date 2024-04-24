d40125daf7a7a653a8357635ed5c3e03d523a015
slhartmann42 <samuelh343@gmail.com>
2024-04-24 18:15:48 +0000

    Lab 13 shell executes

--- update.sh
+++ update.sh
@@ -4,9 +4,10 @@ if [[ $# -eq 0 ]]; then
 	echo 'Sorry no password given so password unchanged'
 
 else
-	mysql -u root --no-password
-	ALTER USER 'root'@'localhost'  IDENTIFIED BY "$1"
-	FLUSH PRIVILEDGES
-	exit
-	echo "Password changed and flushed"
+mysql -u root --skip-password <<EOFT
+ALTER USER 'root'@'localhost' IDENTIFIED BY "$1";
+FLUSH PRIVILEDGES;
+quit;
+EOFT
+echo "Password changed and flushed"
 fi
