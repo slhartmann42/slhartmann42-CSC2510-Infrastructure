6b79c7cc0c1fa9ec1fa3177e1f189754620206fd
slhartmann42 <samuelh343@gmail.com>
2024-04-27 02:27:27 +0000

    Lab 13 done

--- update.sh
+++ update.sh
@@ -4,8 +4,10 @@ if [[ $# -eq 0 ]]; then
 	echo 'Sorry no password given so password unchanged'
 
 else
-mysql -u root  <<EOFT
-ALTER USER 'root'@'localhost' IDENTIFIED BY "$1";
+NEW_PASSWORD="$1"
+
+mysql -u root <<EOFT
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$NEW_PASSWORD';
 FLUSH PRIVILEGES;
 EOFT
 echo "Password changed and flushed"
